13ac8d92b15febb7b6f4ae889bcaf666ec115764
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-10-17 15:01:48 +0200

    Add rpm-build to protected packages in mock
    Sometimes dnf will try to remove it when installing dependencies.
    
    Related #35

--- src/main/java/io/kojan/mbici/tasks/Mock.java
+++ src/main/java/io/kojan/mbici/tasks/Mock.java
@@ -92,6 +92,7 @@ class Mock {
             bw.write("install_weak_deps=" + (installWeakDeps ? 1 : 0) + "\n");
             bw.write("metadata_expire=-1\n");
             bw.write("best=1\n");
+            bw.write("protected_packages=rpm-build\n");
 
             int priority = 0;
             for (Path repoPath : context.getDependencyArtifacts(ArtifactType.REPO)) {
