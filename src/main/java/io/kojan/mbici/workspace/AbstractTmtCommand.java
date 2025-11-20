f1dbaca28ea33dd469e71479d2f732071b6ac815
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-11-20 13:04:49 +0100

    Refactor test and validate commands

--- src/main/java/io/kojan/mbici/workspace/AbstractTmtCommand.java
+++ src/main/java/io/kojan/mbici/workspace/AbstractTmtCommand.java
@@ -30,18 +30,12 @@ public abstract class AbstractTmtCommand extends AbstractCommand {
     @Option(
             names = {"-e", "--environment"},
             description = "Environment to pass to tmt, in format key=val.")
-    private List<String> environment = new ArrayList<>();
+    protected final List<String> environment = new ArrayList<>();
 
     @Option(
             names = {"-c", "--context"},
             description = "Context to pass to tmt, in format key=val.")
-    private List<String> context = new ArrayList<>();
-
-    protected abstract String getTestPlan();
-
-    protected abstract boolean requiresGuest();
-
-    protected abstract boolean reserveSys();
+    protected final List<String> context = new ArrayList<>();
 
     public static Path findComposeOrAbort(Workspace ws) throws IOException, XMLException {
         WorkspaceConfig c = ws.getConfig();
@@ -52,102 +46,5 @@ public abstract class AbstractTmtCommand extends AbstractCommand {
             }
         }
         throw new RuntimeException("This command requires a compose");
-    }
-
-    @Override
-    public Integer call() throws Exception {
-
-        ShellCommand shell = null;
-        if (requiresGuest()) {
-            shell = new ShellCommand();
-            shell.setId("test-" + getTestPlan());
-            Integer ret = shell.provision();
-            if (ret != 0) {
-                return ret;
-            }
-        }
-
-        String testPlan = getTestPlan();
-        if (testPlan.startsWith("/")) {
-            testPlan = testPlan.substring(1);
-        }
-
-        Workspace ws = Workspace.findOrAbort();
-        WorkspaceConfig c = ws.getConfig();
-        info("Using workspace at " + ws.getWorkspaceDir());
-
-        Path composeRepoDir = findComposeOrAbort(ws);
-
-        List<String> cmd = new ArrayList<>();
-        cmd.add("tmt");
-        if (!requiresGuest()) {
-            cmd.add("--feeling-safe");
-        }
-        cmd.add("-r");
-        cmd.add(c.getTestPlanDir().toString());
-        for (String ctx : context) {
-            cmd.add("-c");
-            cmd.add(ctx);
-        }
-
-        cmd.add("run");
-        cmd.add("--scratch");
-        cmd.add("-a");
-        cmd.add("-i");
-        cmd.add(c.getTestResultDir().resolve(testPlan).toString());
-        cmd.add("-e");
-        cmd.add("TEST_ARTIFACTS=" + composeRepoDir);
-        for (String env : environment) {
-            cmd.add("-e");
-            cmd.add(env);
-        }
-
-        cmd.add("plans");
-        cmd.add("--name");
-        cmd.add("^/" + testPlan + "$");
-
-        if (requiresGuest()) {
-            cmd.add("provision");
-            cmd.add("--how");
-            cmd.add("connect");
-            cmd.add("--guest");
-            cmd.add("dummy");
-            cmd.add("--user");
-            cmd.add("root");
-            cmd.add("--ssh-option");
-            cmd.add("ProxyCommand=socat - UNIX-CONNECT:" + shell.getSocketPath());
-            cmd.add("--ssh-option");
-            cmd.add("PreferredAuthentications=password");
-            cmd.add("--ssh-option");
-            cmd.add("PubkeyAuthentication=no");
-            cmd.add("--ssh-option");
-            cmd.add("KbdInteractiveAuthentication=no");
-            cmd.add("--ssh-option");
-            cmd.add("GSSAPIAuthentication=no");
-            cmd.add("--ssh-option");
-            cmd.add("UserKnownHostsFile=/dev/null");
-            cmd.add("--ssh-option");
-            cmd.add("StrictHostKeyChecking=no");
-            cmd.add("-vvv");
-        } else {
-            cmd.add("provision");
-            cmd.add("--how");
-            cmd.add("local");
-        }
-
-        cmd.add("execute");
-        cmd.add("-vvv");
-
-        cmd.add("report");
-        cmd.add("-vv");
-
-        int ret = new ProcessBuilder(cmd).inheritIO().start().waitFor();
-        if (shell != null) {
-            if (reserveSys()) {
-                shell.connect();
-            }
-            shell.terminate();
-        }
-        return ret;
     }
 }
