e94f3d0d61dd2e98bbef68ee55570a1c42e0be36
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-11-20 13:04:49 +0100

    Refactor test and validate commands

--- src/main/java/io/kojan/mbici/workspace/TestCommand.java
+++ src/main/java/io/kojan/mbici/workspace/TestCommand.java
@@ -16,6 +16,9 @@
 package io.kojan.mbici.workspace;
 
 import io.kojan.mbici.Main;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
@@ -36,17 +39,84 @@ public class TestCommand extends AbstractTmtCommand {
     private boolean reserve;
 
     @Override
-    protected String getTestPlan() {
-        return testPlan;
-    }
+    public Integer call() throws Exception {
 
-    @Override
-    protected boolean requiresGuest() {
-        return true;
-    }
+        ShellCommand shell = new ShellCommand();
+        shell.setId("test-" + testPlan);
+        Integer ret = shell.provision();
+        if (ret != 0) {
+            return ret;
+        }
 
-    @Override
-    protected boolean reserveSys() {
-        return reserve;
+        if (testPlan.startsWith("/")) {
+            testPlan = testPlan.substring(1);
+        }
+
+        Workspace ws = Workspace.findOrAbort();
+        WorkspaceConfig c = ws.getConfig();
+        info("Using workspace at " + ws.getWorkspaceDir());
+
+        Path composeRepoDir = findComposeOrAbort(ws);
+
+        List<String> cmd = new ArrayList<>();
+        cmd.add("tmt");
+        cmd.add("-r");
+        cmd.add(c.getTestPlanDir().toString());
+        for (String ctx : context) {
+            cmd.add("-c");
+            cmd.add(ctx);
+        }
+
+        cmd.add("run");
+        cmd.add("--scratch");
+        cmd.add("-a");
+        cmd.add("-i");
+        cmd.add(c.getTestResultDir().resolve(testPlan).toString());
+        cmd.add("-e");
+        cmd.add("TEST_ARTIFACTS=" + composeRepoDir);
+        for (String env : environment) {
+            cmd.add("-e");
+            cmd.add(env);
+        }
+
+        cmd.add("plans");
+        cmd.add("--name");
+        cmd.add("^/" + testPlan + "$");
+
+        cmd.add("provision");
+        cmd.add("--how");
+        cmd.add("connect");
+        cmd.add("--guest");
+        cmd.add("dummy");
+        cmd.add("--user");
+        cmd.add("root");
+        cmd.add("--ssh-option");
+        cmd.add("ProxyCommand=socat - UNIX-CONNECT:" + shell.getSocketPath());
+        cmd.add("--ssh-option");
+        cmd.add("PreferredAuthentications=password");
+        cmd.add("--ssh-option");
+        cmd.add("PubkeyAuthentication=no");
+        cmd.add("--ssh-option");
+        cmd.add("KbdInteractiveAuthentication=no");
+        cmd.add("--ssh-option");
+        cmd.add("GSSAPIAuthentication=no");
+        cmd.add("--ssh-option");
+        cmd.add("UserKnownHostsFile=/dev/null");
+        cmd.add("--ssh-option");
+        cmd.add("StrictHostKeyChecking=no");
+        cmd.add("-vvv");
+
+        cmd.add("execute");
+        cmd.add("-vvv");
+
+        cmd.add("report");
+        cmd.add("-vv");
+
+        int tmtRet = new ProcessBuilder(cmd).inheritIO().start().waitFor();
+        if (reserve) {
+            shell.connect();
+        }
+        shell.terminate();
+        return tmtRet;
     }
 }
--- src/main/java/io/kojan/mbici/workspace/ValidateCommand.java
+++ src/main/java/io/kojan/mbici/workspace/ValidateCommand.java
@@ -16,6 +16,9 @@
 package io.kojan.mbici.workspace;
 
 import io.kojan.mbici.Main;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import picocli.CommandLine.Command;
 
 @Command(
@@ -25,18 +28,60 @@ import picocli.CommandLine.Command;
         versionProvider = Main.class)
 public class ValidateCommand extends AbstractTmtCommand {
 
-    @Override
-    protected String getTestPlan() {
-        return "validate";
-    }
+    private final String planName = "validate";
 
     @Override
-    protected boolean requiresGuest() {
-        return false;
-    }
+    public Integer call() throws Exception {
 
-    @Override
-    protected boolean reserveSys() {
-        return false;
+        String testPlan = planName;
+        if (testPlan.startsWith("/")) {
+            testPlan = testPlan.substring(1);
+        }
+
+        Workspace ws = Workspace.findOrAbort();
+        WorkspaceConfig c = ws.getConfig();
+        info("Using workspace at " + ws.getWorkspaceDir());
+
+        Path composeRepoDir = findComposeOrAbort(ws);
+
+        List<String> cmd = new ArrayList<>();
+        cmd.add("tmt");
+        if (!false) {
+            cmd.add("--feeling-safe");
+        }
+        cmd.add("-r");
+        cmd.add(c.getTestPlanDir().toString());
+        for (String ctx : context) {
+            cmd.add("-c");
+            cmd.add(ctx);
+        }
+
+        cmd.add("run");
+        cmd.add("--scratch");
+        cmd.add("-a");
+        cmd.add("-i");
+        cmd.add(c.getTestResultDir().resolve(testPlan).toString());
+        cmd.add("-e");
+        cmd.add("TEST_ARTIFACTS=" + composeRepoDir);
+        for (String env : environment) {
+            cmd.add("-e");
+            cmd.add(env);
+        }
+
+        cmd.add("plans");
+        cmd.add("--name");
+        cmd.add("^/" + testPlan + "$");
+
+        cmd.add("provision");
+        cmd.add("--how");
+        cmd.add("local");
+
+        cmd.add("execute");
+        cmd.add("-vvv");
+
+        cmd.add("report");
+        cmd.add("-vv");
+
+        return new ProcessBuilder(cmd).inheritIO().start().waitFor();
     }
 }
